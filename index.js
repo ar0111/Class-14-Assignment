81eabea5f3292dd7ebc037a500d341ba09538d5f
Arafat-Rahman <arafat.cuet99@gmail.com>
2023-06-25 22:34:49 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,67 @@
+// console.log("Hello World");
+
+var address = "Shyamoli";
+// console.log(address);
+
+var student1 = "Sakib Khan";
+// console.log(student1, address);
+
+var price = 20;
+// console.log(price, student1);
+
+var doYouLoveMe = true;
+// console.log(doYouLoveMe);
+
+var fullNameOfAbdul = "Abdul Kader";
+fullNameOfAbdul = "Abdul Kader Samad";
+// console.log(fullNameOfAbdul);
+
+var PIE = 3.1416;
+// console.log(PIE);
+
+//Mathemetical Operation
+
+var moneyOfSakib = 200;
+var moneyOfTamim = 300;
+
+var total = moneyOfSakib + moneyOfTamim;
+var difference = moneyOfTamim - moneyOfSakib;
+var divison = moneyOfTamim / moneyOfSakib;
+var remaining = moneyOfTamim % moneyOfSakib;
+var multiply = moneyOfTamim * moneyOfSakib;
+// console.log(total);
+// console.log(difference);
+// console.log(divison);
+// console.log(remaining);
+// console.log(multiply);
+
+var priceOfOrange = 200;
+// priceOfOrange = priceOfOrange + 50;
+// priceOfOrange += 50;
+// priceOfOrange -= 50;
+priceOfOrange *= 50;
+// var newPriceOfOrange = priceOfOrange + 50;
+// console.log(newPriceOfOrange);
+// console.log(priceOfOrange);
+
+var firstName = "Abdul ";
+var lastName = "Kader";
+
+var fullName = firstName + lastName;
+// console.log(fullName);
+
+var price1 = "55.52";
+var price2 = "50";
+
+var numberPrice1 = parseFloat(price1);
+var numberPrice2 = parseInt(price2);
+// console.log((numberPrice1 + numberPrice2).toFixed(2));
+var totalNumberPrice = numberPrice1 + numberPrice2;
+// console.log(totalNumberPrice.toFixed(2));
+
+// console.log(typeof totalNumberPrice);
+
+var num1 = 0.1;
+var num2 = 0.2;
+
+console.log(parseFloat((num1 + num2).toFixed(1)));
